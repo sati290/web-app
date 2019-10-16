110eadd02f4326d5ce4586acab8fff756c45d2b0
Attila Simon <sati290@gmail.com>
2019-10-16 21:38:42 +0200

    create login form

--- default/src/App.js
+++ default/src/App.js
@@ -2,11 +2,59 @@ import React from 'react';
 import './App.css';
 
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
-  return (
-    <div>Hello World!</div>
-  );
+class LoginForm extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit() {
+
+  }
+
+  render() {
+    return (
+      <Form onSubmit={() => this.handleSubmit()}>
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" type="email" onChange={e => this.handleChange(e)}/>
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control name="password" type="password" onChange={e => this.handleChange(e)}/>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    )
+  }
 }
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    user: {}
+  }
+
+  handleLogin(event) {
+  }
+
+  render() {
+    const { name } = this.state.user
+
+    return (
+      <div>
+        {name
+          ? "Logged in as " + name
+          : <LoginForm onSubmit={e => this.handleLogin(e)} />
+        }
+      </div>
+    )
+  }
+}
