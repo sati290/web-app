f35d25b78dd600f99faa795353f35a6889075626
Attila Simon <sati290@gmail.com>
2019-10-22 23:16:38 +0200

    eslint auto fixes

--- website/src/App.js
+++ website/src/App.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import { Dialog, DialogTitle, TextField, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  Dialog, DialogTitle, TextField, DialogContent, DialogContentText, DialogActions,
+} from '@material-ui/core';
 
 function LoginDialog(props) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState(null)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const handleSubmit = event => {
+    event.preventDefault();
 
     firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-      props.onClose()
+      props.onClose();
     }).catch(error => {
-      setError(error.message)
-    })
-  }
+      setError(error.message);
+    });
+  };
 
   return (
     <Dialog open={props.open} onClose={props.onClose}>
@@ -32,86 +34,85 @@ function LoginDialog(props) {
         <DialogContentText>
           Enter your credentials to login.
         </DialogContentText>
-        <form id='login-form' style={{display: 'flex', flexDirection: 'column'}} onSubmit={handleSubmit}>
-          <TextField label="Email" type="email" onChange={e => setEmail(e.target.value)}/>
-          <TextField label="Password" type="password" onChange={e => setPassword(e.target.value)}/>
+        <form id="login-form" style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
+          <TextField label="Email" type="email" onChange={e => setEmail(e.target.value)} />
+          <TextField label="Password" type="password" onChange={e => setPassword(e.target.value)} />
         </form>
         {error && <DialogContentText>{error}</DialogContentText>}
       </DialogContent>
       <DialogActions>
         <Button onClick={props.onClose}>Cancel</Button>
-        <Button form='login-form' type='submit'>Submit</Button>
+        <Button form="login-form" type="submit">Submit</Button>
       </DialogActions>
     </Dialog>
-  )
+  );
 }
 
 function LoginButton(props) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   return (
     <>
       <Button onClick={() => setOpen(true)} {...props}>Login</Button>
       <LoginDialog open={open} onClose={handleClose} />
     </>
-  )
+  );
 }
 
 class RestTest extends React.Component {
   state = {
-    message: ""
+    message: '',
   }
 
   componentDidMount() {
-    fetch("https://test-web-app-255809.web.app/api/helloWorld")
+    fetch('https://test-web-app-255809.web.app/api/helloWorld')
       .then(res => res.text())
-      .then(data => this.setState({message: data}))
-      .catch(console.log)
+      .then(data => this.setState({ message: data }))
+      .catch(console.log);
   }
 
   render() {
     return (
       <Typography>{this.state.message}</Typography>
-    )
+    );
   }
 }
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      user: null
-    }
+      user: null,
+    };
 
 
-    firebase.auth().onAuthStateChanged(user =>{
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.setState({user: user})
+        this.setState({ user });
       } else {
-        this.setState({user: null})
+        this.setState({ user: null });
       }
-    })
+    });
   }
 
   render() {
     return (
       <>
-        <AppBar position='static'>
+        <AppBar position="static">
           <Toolbar>
-            <Typography variant='h6' style={{flexGrow: 1}}>Test Web App</Typography>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>Test Web App</Typography>
             {this.state.user
-              ? <Button color='inherit' onClick={() => firebase.auth().signOut()}>Logout</Button>
-              : <LoginButton color='inherit' />
-            }
+              ? <Button color="inherit" onClick={() => firebase.auth().signOut()}>Logout</Button>
+              : <LoginButton color="inherit" />}
           </Toolbar>
         </AppBar>
         <RestTest />
       </>
-    )
+    );
   }
 }
