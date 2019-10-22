73488e47764e1c6c765c16ed31d7bbe0bf64687a
Attila Simon <sati290@gmail.com>
2019-10-22 13:38:51 +0200

    make auth signIn async function

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -9,33 +9,32 @@ const db = admin.firestore()
 router.use(express.json())
 router.use(cors(origin=true))
 
-router.post('/signIn', (req, res) => {
-  db.collection('users').where('email', '==', req.body.email).get()
-    .then(snapshot => {
-      if(snapshot.empty) {
-        res.sendStatus(401)
-      } else if (snapshot.size != 1) {
-        res.sendStatus(500)
-      } else {
-        const doc = snapshot.docs[0]
-        const data = doc.data()
+router.post('/signIn', async (req, res) => {
+  try {
+    const snapshot = await db.collection('users').where('email', '==', req.body.email).get()
 
-        bcrypt.compare(req.body.password, data.password).then(result => {
-          if(result == true) {
-            res.json({
-              id: doc.id,
-              email: data.email
-            })
-          } else {
-            res.sendStatus(401)
-          }
-        })
-      }
-    })
-    .catch(error => {
-      console.log('error:', error)
+    if(snapshot.empty) {
+      res.sendStatus(401)
+    } else if (snapshot.size !== 1) {
       res.sendStatus(500)
-    })
+    } else {
+      const doc = snapshot.docs[0]
+      const data = doc.data()
+
+      const result = await bcrypt.compare(req.body.password, data.password)
+      if(result === true) {
+        res.json({
+          id: doc.id,
+          email: data.email
+        })
+      } else {
+        res.sendStatus(401)
+      }
+    }
+  } catch(err) {
+      console.log('error:', err)
+      res.sendStatus(500)
+  }
 })
 
 exports.router = router
