0e431942c27649c928738fa3ade0ec8b71acf89a
Attila Simon <sati290@gmail.com>
2019-10-22 20:56:51 +0200

    revert auth service
    Revert "auth signIn returns JWT"
    
    This reverts commit 5fa3c6c32fc3d0a4e3822a36a0adb1789c03527f.
    
    Revert "make auth signIn async function"
    
    This reverts commit 73488e47764e1c6c765c16ed31d7bbe0bf64687a.
    
    Revert "create signIn function in auth"
    
    This reverts commit 36568abd53f5cefdd53b58e4fe7a0729d069b6cf.
    
    Revert "auth service"
    
    This reverts commit e0a4770162074186021f371050a828508ce541d7.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
-const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const express = require('express')
 
-admin.initializeApp(functions.config().firebase);
-
-exports.auth = functions.https.onRequest(express().use('/api/auth', require('./auth').router))
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.send("Hello from Firebase!");
+});
