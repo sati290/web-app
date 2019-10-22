93b26a6f26e006b52a6856df5df626cd559c844b
Attila Simon <sati290@gmail.com>
2019-10-22 22:27:26 +0200

    move firebase init to index.js

--- website/src/App.js
+++ website/src/App.js
@@ -10,16 +10,6 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import { Dialog, DialogTitle, TextField, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID
-};
-
 function LoginDialog(props) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -98,7 +88,6 @@ export default class App extends React.Component {
       user: null
     }
 
-    firebase.initializeApp(firebaseConfig)
 
     firebase.auth().onAuthStateChanged(user =>{
       if (user) {
