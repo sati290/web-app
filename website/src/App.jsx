f8ecca353515e54a0f92979a99b114230c26cf0f
Attila Simon <sati290@gmail.com>
2019-10-28 19:28:37 +0100

    move firebase init into a separate file

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
 import {
   Dialog, DialogTitle, TextField, DialogContent, DialogContentText, DialogActions,
 } from '@material-ui/core';
+import * as firebase from './Firebase';
 
 function LoginDialog(props) {
   const [email, setEmail] = useState('');
